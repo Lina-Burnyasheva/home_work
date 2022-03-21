ce300859128b02969a4e60f783e1918a67f9a15d
Lina-Burnyasheva <100047736+Lina-Burnyasheva@users.noreply.github.com>
2022-03-21 15:09:16 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -22,39 +22,3 @@ for i in range(100):
     else:
         print('{} процентов'.format(i + 1))
 
-
-# задание 2
-# """output:
-#     sum of odd elem which % 7
-#     sum of odd elem(xyz..) sum(x+y+z) which % 7
-#     sum of odd (elem + 17) which % 7
-# """
-#
-# def sum_of_num(num:int):
-#     res = 0
-#
-#     while num > 0:
-#         res += num % 10
-#         num //= 10
-#     return res
-#
-# # lazy list
-# list_of_odd = range(1, 1000 + 1, 2)
-#
-# sum_with_17:int = 0
-# sum_without_17:int = 0
-# for elem in list_of_odd:
-#     cube = elem ** 3
-#     cube_with_17 = (elem + 17) ** 3
-#
-#     if cube % 7 == 0:
-#     if sum_of_num(cube) % 7 == 0:
-#         sum_without_17 += cube
-#
-#     if cube_with_17 % 7 == 0:
-#     if sum_of_num(cube_with_17) % 7 == 0:
-#         sum_with_17 += cube_with_17
-#
-# print("сумма без 17:", sum_without_17)
-# print("сумма c 17:", sum_with_17)
-#
